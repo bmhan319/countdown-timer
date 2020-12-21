beeec0f3ae2f7366f31f41b402e14d99e399001a
Brian Han <brianhan@brians-mbp.lan>
2020-12-21 01:45:49 -0800

    added bg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Social from './components/Social'
+import './css/social.css'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        hi
+      <div className="App">
+        <Social />
       </div>
     )
   }
---
+++ src/components/Social.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Social() {
+  return (
+    <div className="socialComponent">
+      <div className="socialContainer">
+        <div className="socialIcon socialFaceBook">FB</div>
+        <div className="socialIcon socialPinterest">Pinterest</div>
+        <div className="socialIcon socialInstagram">Instagram</div>
+      </div>
+    </div>
+  )
+}
